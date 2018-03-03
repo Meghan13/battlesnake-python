9a59bce2a377e198c7f930bc79e0bc2b4ac08e44
AbstractAlgebra <Coxdallas@gmail.com>
2018-03-03 12:38:56 -0800

    Basic snake

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import bottle
 import os
 import random
-
+import json
 
 
 @bottle.route('/')
@@ -17,6 +17,8 @@ def static(path):
 @bottle.post('/start')
 def start():
     data = bottle.request.json
+    #print bottle
+    #print "TEST"
     game_id = data.get('game_id')
     board_width = data.get('width')
     board_height = data.get('height')
@@ -31,7 +33,8 @@ def start():
     return {
         'color': '#00FF00',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
-        'head_url': head_url
+        'head_url': head_url,
+        'name': 'dallas'
     }
 
 
@@ -40,13 +43,40 @@ def move():
     data = bottle.request.json
 
     # TODO: Do things with data
+    print
+    print
+    #x = json.loads(data)
+    for foods in data['food']['data']:
+        print foods['x']
+        print foods['y']
+        print
+
+    goalFood = data['food']['data'][0]
+
+    #print json.dumps(data,indent=4) 
     
+    currPosHeadX = data['you']['body']['data'][0]['x']
+    currPosHeadY = data['you']['body']['data'][0]['y']
+
     directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
-    print direction
+    #direction = random.choice(directions)
+
+    if(goalFood['x'] < currPosHeadX):
+        direction = 'left'
+    
+    if(goalFood['x'] > currPosHeadX):
+        direction = 'right'
+    
+    if(goalFood['y'] < currPosHeadY):
+        direction = 'up'
+    
+    if(goalFood['y'] > currPosHeadY):
+        direction = 'down'
+
+    #print direction
     return {
         'move': direction,
-        'taunt': 'battlesnake-python!'
+        'taunt': 'For the Horde!'
     }
 
 
